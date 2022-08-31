536b5e684df0f1c3f3fa02a3a7ce1b4e649e494f
Luigi Marino <emails@luigi-marino.com>
2022-08-31 10:31:57 +0000

    Getting messy, going to rewrite to avoid confusion

--- anagram_check.py
+++ anagram_check.py
@@ -1,24 +1,21 @@
 from english_words import english_words_set
 
 def check_anagram(anagram, word):
-
     looper = 0
     anagram_check = True
 
-    for i in word:
-        if anagram[looper] in word:
-            continue
-        else:
-            anagram_check = False
-        looper += 1
-    return anagram_check
+    if len(anagram) == len(word):
+        for i in word:
+            if i not in word:
+                anagram_check = False
+            looper += 1
+            print(i)
+        print("{} is {}".format(word, anagram_check))
+    else:
+        exit
+    #return anagram_check
 
-word_1 = "test"
-anagram_1 = "sett"
+anagram = 'sett'
 
-word_2 = "random"
-anagram_2 = "settah"
-
-
-print(check_anagram(anagram_1, word_1))
-print(check_anagram(anagram_2, word_2))
+for i in english_words_set:
+    check_anagram(anagram, i)
