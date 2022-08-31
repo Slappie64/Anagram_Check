9ec4f89c8a77380d6ba37ddf885b27690a7c9f32
Luigi Marino <emails@luigi-marino.com>
2022-08-31 13:01:03 +0000

    Documentation and code readability

--- anagram_check.py
+++ anagram_check.py
@@ -1,11 +1,16 @@
 import sys
 from english_words import english_words_set
 
+# Take an argument from the command line.
+# Remove white space and make lowercase.
 anagram = sys.argv[1].lower().replace(' ', '')
+# Create an empty list for potential solved words.
 potential_words = []
 
-print(potential_words)
-
+# Function
+# Takes a word and checks each letter against the anagram.
+# If the letter does not exist in both, move on.
+# If it does, append the Potential_Words list with the word.
 def check_anagram(word, anagram):
     check = True
 
@@ -16,9 +21,14 @@ def check_anagram(word, anagram):
     if check == True:        
         potential_words.append(word)
 
+
+# Loop
+# For each word in the english_words_list.
+# Check whether the word is the same length as the anagram.
+# Then run a letter by letter check against the anagram.
 for word in english_words_set:
     if len(anagram) == len(word):
         check_anagram(word, anagram)
 
+# Print out the list of potential words
 print(potential_words)
-print(anagram)
