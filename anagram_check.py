b2a16290ca05119ae7e79cdeb813877605a78536
Luigi Marino <emails@luigi-marino.com>
2022-08-31 09:38:33 +0000

    Created a loop that successfully tests for anagrams

--- anagram_check.py
+++ anagram_check.py
@@ -1,10 +1,13 @@
-word = "Test"
-anagram = "SETT"
+word = "test"
+anagram = "sett"
 looper = 0
+anagram_check = True
 
 for i in word:
     if anagram[looper] in word:
-        continue
+        print('Found')
     else:
-        print("Nope")
-    print("Yup")
+        anagram_check = False
+    looper += 1
+
+print(anagram_check)
