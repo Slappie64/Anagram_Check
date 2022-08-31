3247e69570d2613d5b698f3501cbd2351100728d
Luigi Marino <emails@luigi-marino.com>
2022-08-31 09:55:20 +0000

    Added a list of English Words to compare against

--- anagram_check.py
+++ anagram_check.py
@@ -1,13 +1,25 @@
+from english_words import english_words_set
+
 word = "test"
 anagram = "sett"
-looper = 0
-anagram_check = True
 
-for i in word:
-    if anagram[looper] in word:
-        print('Found')
-    else:
-        anagram_check = False
-    looper += 1
+
+
+def check_anagram(anagram, word):
+
+    looper = 0
+    anagram_check = True
+
+    for i in word:
+        if anagram[looper] in word:
+            print('Found')
+        else:
+            anagram_check = False
+        looper += 1
+        
+    return anagram_check
 
 print(anagram_check)
+
+for i in english_words_set:
+    print(i)
